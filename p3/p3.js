e15bc0c80d0798a0627cd7c034e9974622f6dd31
aliceyoung1997 <alice06181997@gmail.com>
2021-07-24 17:07:23 +0800

    2 files modified

--- p3/p3.js
+++ p3/p3.js
@@ -9,10 +9,30 @@ fetch('https://jsonplaceholder.typicode.com/users')
       }
 
       response.json().then(function(data) {
-        console.log(data)
+        appendData(data)
       })
     }
   )
   .catch(function(err) {
     console.log('Fetch Error :-S', err)
   })
+
+  function appendData(data){
+    for(let i = 0; i < data.length; i++){
+      let name=data[i].name
+      let email=data[i].email
+      let company= data[i].company.name
+      const li =document.createElement('li')
+      li.classList.add('person')
+      li.innerHTML=`${name}/${company}/${email} <button class='delete'>刪除</button>`
+      document.querySelector('.list').appendChild(li)
+    }
+  }
+
+  document.querySelector('.list').addEventListener('click',
+    function(e){
+      if(e.target.classList.contains('delete')){
+        document.querySelector('.list').removeChild(e.target.closest('.person'))
+      }
+    }
+  )
